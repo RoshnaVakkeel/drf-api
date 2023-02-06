25ebb007c837f2a0957fa1b051734a3a67ff6224
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-02-06 11:31:31 +0000

    Add filters to post and profile

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post
 from .serializers import PostSerializer
 
@@ -17,14 +18,24 @@ class PostList(generics.ListCreateAPIView):
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]   
+    search_fields = [
+        'owner__username',
+        'title',
     ]
     ordering_fields = [
         'likes_count',
         'comments_count',
         'likes__created_at',
     ]
-
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
